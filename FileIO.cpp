bbe7b4472c44df81f70d446e447d2f7ae6a2a5f7
Vishal Fenn <vishal.fenn.18@cnu.edu>
2020-11-15 12:03:25 -0500

    Completed fileio

--- FileIO.cpp
+++ FileIO.cpp
@@ -1,8 +1,8 @@
 /*
  * FileReader.cpp
  *
- *  Created on: Oct 8, 2017
- *      Author: keith
+ *  Created on: Nov 11, 2020
+ *      Author: Vishal Fenn
  */
 #include <iostream>
 #include <fstream>
@@ -11,14 +11,43 @@
 
 using namespace std;
 
-int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
-{
-	//TODO fill in
+int KP_FileIO::getFileContents(const string &filename,
+		string &contents){
+	ifstream cont_file;
+	cont_file.open(contents);
+	if (!cont_file) {
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+	contents.clear();
+	string lineFeed;
+	while (cont_file.peek() != EOF) {
+		getline(cont_file, lineFeed);
+		size_t reveal = lineFeed.find('\n');
+		if (reveal != -1) {
+			lineFeed.erase(reveal, 1);
+		}
+		contents += lineFeed;
+	}
+	if (contents.empty()) {
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+	return SUCCESS;
+
+
 }
 
-int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
-{
-	//TODO fill in
+int KP_FileIO::writeVectortoFile(const string filename,
+		vector<string> &myEntryVector){
+	ofstream vect_file;
+	vect_file.open(filename.c_str());
+	if (!vect_file) {
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+	}
+	for (string entries : myEntryVector) {
+		vect_file << entries << '\n';
+	}
+	vect_file.close();
+	return SUCCESS;
 }
 
 
